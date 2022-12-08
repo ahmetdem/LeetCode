0fd95ceb687ef7662d344327963520975dadaa61
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2022-12-08 14:38:13 +0300

    q-728

--- Q-728_Self_Dividing_Numbers.py
+++ Q-728_Self_Dividing_Numbers.py
@@ -1,30 +1,32 @@
-left = 5
-right = 100
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
 
-sequence = list(range(left, right))
+        def getDig(num):
+            diglist = []
+            
+            while num > 0:
+                lastDig = num % 10
+                diglist.append(lastDig)
+                num //= 10
+            
+            return diglist
 
-def getDig(num):
-    diglist = []
-    
-    while num > 0:
-        lastDig = num % 10
-        diglist.append(lastDig)
-        num //= 10
-    
-    return diglist
 
-def isSelf(num):
     digitlist = getDig(num)
+        def isSelf(num):
+            digitlist = getDig(num)
+            if 0 in digitlist:
+                return False
+            for i in digitlist:
+                if num % i != 0:
+                    break
+            else: return True 
 
-    if 0 in digitlist:
-        return False
-    
-    for i in digitlist:
-        if num % i != 0:
-            break
-    else: return True 
-    
+        sequence = list(range(left, right+1))
 
-answer = list(filter(isSelf, sequence))
-
-print(answer)    
+        return list(filter(isSelf, sequence))
