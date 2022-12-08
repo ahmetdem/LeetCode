0d916007069d37a317026c47a6b2f3f4d3d366a0
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2022-12-08 22:13:24 +0300

    q977

--- Q-977_Squares_of_a_Sorted_Array.py
+++ Q-977_Squares_of_a_Sorted_Array.py
@@ -1,7 +1,11 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
 
+        res = [x*x for x in nums]
+        res.sort()
 
-nums = [-4,-1,0,3,10]
-
-res = [x*x for x in nums]
-res.sort()
-print(res)
+        return res
