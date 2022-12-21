0fc35baac889f59d279c5125ad4207fde9873818
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2022-12-21 21:56:53 +0300

    Create Q-69_Sqrt.py

---
+++ Q-69_Sqrt.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    import math 
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        if x == 0:
+            return 0
+
+        guess = 1.0
+        while abs(guess*guess - x) > 0.5:
+            guess = (guess + x/guess)/2
+        return int(math.floor(guess))
