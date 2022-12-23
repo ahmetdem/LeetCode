f657088bc399279a0085fe957754651d0145547e
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2022-12-23 20:20:39 +0300

    ssda

--- Q-344_Reverse_String.py
+++ Q-344_Reverse_String.py
@@ -12,3 +12,17 @@ class Solution(object):
                 
         return s
     
+    
+        """
+        def reverse_string(s):
+            # Initialize left and right indices
+            left = 0
+            right = len(s) - 1
+
+            # Swap elements until the indices meet in the middle
+            while left < right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1 
+        """
+    
