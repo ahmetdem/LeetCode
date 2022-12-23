2b180a3be728677c435f1f4f977ee308f7f03c7f
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2022-12-23 21:04:36 +0300

    Update Q-1704_Determine_if_String_Halves_Are_Alike.py

--- Q-1704_Determine_if_String_Halves_Are_Alike.py
+++ Q-1704_Determine_if_String_Halves_Are_Alike.py
@@ -1,21 +1,26 @@
-s = "Uo"
-
-lenght = len(s)
-
-
-s1 = s[int(lenght/2):]
-s2 = s[:int(lenght/2)]
-
-def findVower(sentence):
-    vowels = ["a", "e", "i", "o", "u"]
-    sentence = sentence.lower()
-    vowelCount = 0
+class Solution(object):
+    def halvesAreAlike(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        
+        lenght = len(s)
+        
+        s1 = s[int(lenght/2):]
+        s2 = s[:int(lenght/2)]
     
-    for word in sentence:
-        if word in vowels:
-            vowelCount += 1
-    return vowelCount
-
-if findVower(s1) == findVower(s2):
-    print(True)
-else: print(False)
+        def findVower(sentence):
+            vowels = ["a", "e", "i", "o", "u"]
+            sentence = sentence.lower()
+            vowelCount = 0
+            
+            for word in sentence:
+                if word in vowels:
+                    vowelCount += 1
+            return vowelCount
+        
+        if findVower(s1) == findVower(s2):
+            return True
+        else: return False
