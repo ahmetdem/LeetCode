01c03cc30ef46aff2ecd73ed39a5344bf1427734
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2022-12-23 21:22:52 +0300

    Create Q-1450_Number_of_Students_Doing_Homework_at_a_Given_Time.py

---
+++ Q-1450_Number_of_Students_Doing_Homework_at_a_Given_Time.py
@@ -0,0 +1,20 @@
+
+startTime = [4]
+endTime = [4]
+queryTime = 4
+
+result = 0
+print(list(range(4,4)))
+
+for i in range(len(startTime)):
+    
+    
+
+        
+    if queryTime in range(startTime[i], endTime[i]):
+        result += 1
+    else:
+        if startTime[0] == 4:
+            result += 1
+
+else: print(result)
