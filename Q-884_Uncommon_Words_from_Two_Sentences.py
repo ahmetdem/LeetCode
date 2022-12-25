c947da997d1281b91091794eb6c2510a32a69894
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2022-12-25 12:12:58 +0300

    Update Q-884_Uncommon_Words_from_Two_Sentences.py

--- Q-884_Uncommon_Words_from_Two_Sentences.py
+++ Q-884_Uncommon_Words_from_Two_Sentences.py
@@ -8,6 +8,6 @@ class Solution(object):
         l1 = s1.split(" ")
         l2 = s2.split(" ")
 
-        return [word for word in l1 if word not in l2 and l1.codeunt(word) == 1] + [word for word in l2 if word not in l1 and l2.count(word) == 1]
+        return [word for word in l1 if word not in l2 and l1.count(word) == 1] + [word for word in l2 if word not in l1 and l2.count(word) == 1]
             
     
